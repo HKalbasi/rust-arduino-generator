10132139ae0b44b596e4a79a8cf298317672b897
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2021-03-09 19:33:50 +0330

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+{% if device == "arduino-uno" %}
+use arduino_uno as device;
+{% endif %}{% if device == "arduino-leonardo" %}
+use arduino_leonardo as device;
+{% endif %}
+
+use device::prelude::*;
+
+// Pull in the panic handler from panic-halt
+use panic_halt as _;
+
+#[device::entry]
+fn main() -> ! {
+    let dp = device::Peripherals::take().unwrap();
+{% if device == "arduino-uno" %}
+    let mut pins = device::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
+{% endif %}{% if device == "arduino-leonardo" %}
+    let mut pins = device::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF);
+{% endif %}
+
+    loop {
+        // TODO: Your code here
+    }
+}
